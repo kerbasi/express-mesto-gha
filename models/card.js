4574cc678411ddd09d0399a04ae5999d9939b7dc
Kirill <kirill.novikov@gmial.com>
2023-06-04 10:38:30 +0300

    install express, add shemes for card and user

---
+++ models/card.js
@@ -0,0 +1,28 @@
+﻿const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("card", userSchema);
