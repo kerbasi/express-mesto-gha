cf8ad100b806f4a17ecde403adbd4eb04c0b12c6
Kirill Novikov <kirill.novikov@gmail.com>
2023-06-17 22:36:49 +0300

    Install limiter

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
+
+app.use(limiter);
 
 app.use((req, res, next) => {
   req.user = {
