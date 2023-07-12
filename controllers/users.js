d66d921893cc2cc7a6845f15fbd059e38c57b82a
Kirill <kirill.novikov@gmial.com>
2023-07-12 23:42:10 +0300

    Fix req deleted

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ module.exports.createUser = (req, res, next) => {
     });
 };
 
-module.exports.findAllUsers = (res, next) => {
+module.exports.findAllUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
     .catch(next);
